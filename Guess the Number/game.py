be1ea3e9007d08e7a5896898695643963ec13a1d
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-12 11:31:57 -0500

    fixed bugs in guess the number game

--- Guess the Number/game.py
+++ Guess the Number/game.py
@@ -27,11 +27,17 @@ def game():
   print("I'm thinking of a number between 1 and 100.")
   answer = randint(1, 100)
   turns = set_difficulty()
-  print(f"You have {turns} attempts remaining to guess the number.")
+  
 
   guess = 0
   while guess != answer:
+    print(f"You have {turns} attempts remaining to guess the number.")
     guess = int(input("Make a guess: "))
     turns = check_answer(guess, answer, turns)
+    if turns == 0:
+      print("You've run out of guesses, you lose")
+      return
+    elif guess != answer:
+      print("Guess again")
 
 game()
