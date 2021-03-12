967fb23cbba930f96ae66731206f3a05eef0337e
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-12 17:00:09 -0500

    pseudocode, break down the problems

---
+++ Higher_Lower_Game/main.py
@@ -0,0 +1,21 @@
+#Display ASCII art
+
+# Generate random IG accounts from list
+
+# Format the account data when printed
+
+# Ask the user for a guess
+
+# Check answer is correct
+  # Check follower count
+      # Use if statement
+
+# Give user feedback on their guess
+
+# score keeping
+
+# Make the game repeatable using a while loop
+
+# Make the guess move up to the top position and set up comparison for the next account
+
+# clear screen between rounds
