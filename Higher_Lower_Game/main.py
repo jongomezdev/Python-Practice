1d5062365dcd92b953b24832a140b2f44e785deb
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-12 17:35:51 -0500

    create check answer function

--- Higher_Lower_Game/main.py
+++ Higher_Lower_Game/main.py
@@ -1,8 +1,26 @@
-#Display ASCII art
-
-from art import logo
+#Import 
+from art import logo, vs
 from game_data import data
 import random
+
+
+def format_data(account):
+  # Create function for less repeated code 
+  """Takes the account data and returns the printable format"""
+  account_name = account["name"]
+  account_desc = account["description"]
+  account_country = account["country"]
+  return f"{account_name}, a {account_desc}, from {account_country}"
+
+def check_answer(guess, a_followers, be_followers):
+  """Use if statement to check if user is correct."""
+  if a_followers > be_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+
+#Display ASCII art
 print(logo)
 
 # Generate random IG accounts from list
@@ -13,17 +31,20 @@ account_b = random.choice(data)
 if account_a == account_b:
   account_b = random.choice(data)
 
-# Format the account data when printed
-
-account_name = account_a["name"]
-account_desc = account_a["description"]
-account_country = account_a["country"]
+print(f"Compare A: {format_data(account_a)}.")
+print(vs)
+print(f"Compare B: {format_data(account_b)}.")
 
 # Ask the user for a guess
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
 # Check answer is correct
   # Check follower count
       # Use if statement
+
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+
 
 # Give user feedback on their guess
 
