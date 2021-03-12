f830972113d8d11e2d913b68d551feab2674e4d7
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-12 17:09:58 -0500

    import, generate accounts, format data to user

--- Higher_Lower_Game/main.py
+++ Higher_Lower_Game/main.py
@@ -1,8 +1,23 @@
 #Display ASCII art
 
+from art import logo
+from game_data import data
+import random
+print(logo)
+
 # Generate random IG accounts from list
 
+account_a = random.choice(data)
+account_b = random.choice(data)
+#conditional to check if accounts are the same, if so make them different
+if account_a == account_b:
+  account_b = random.choice(data)
+
 # Format the account data when printed
+
+account_name = account_a["name"]
+account_desc = account_a["description"]
+account_country = account_a["country"]
 
 # Ask the user for a guess
 
