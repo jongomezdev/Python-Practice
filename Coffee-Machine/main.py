196559380f1f012723438dbbb28fa4fe361ab93b
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-16 17:07:47 -0400

    story logic into functions and variables

--- Coffee-Machine/main.py
+++ Coffee-Machine/main.py
@@ -24,8 +24,31 @@ MENU = {
     }
 }
 
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+
+def is_resource_sufficient(order_ingredients):
+  for item in order_ingredients:
+      if order_ingredients[item] >= resources[item]:
+        print(f"Sorry there is not enough {item}.")
+        return False
+  return True
+
+is_on = True
+
+while is_on:
+  choice = input("What would you like? (espresso/latte/cappucino): ")
+  if choice == "off":
+    is_on = False
+  elif choice == "report":
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${profit}")
+  else:
+    drink = MENU[choice]
+    is_resource_sufficient(drink)
