607a20a3c94f8562efc7c6dc1163a85c733747e8
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-16 17:38:46 -0400

    finish coffee machine logic

--- Coffee-Machine/main.py
+++ Coffee-Machine/main.py
@@ -32,11 +32,39 @@ resources = {
 }
 
 def is_resource_sufficient(order_ingredients):
+  """Returns True when order can be made, False if ingredients are insufficient"""
   for item in order_ingredients:
       if order_ingredients[item] >= resources[item]:
         print(f"Sorry there is not enough {item}.")
         return False
   return True
+
+def process_coins():
+  """Returns the total calculated from coins inserted."""
+  print("Please insert coines.")
+  total = int(input("how many quarters?: ")) * 0.25
+  total += int(input("how many nickels?: ")) * 0.05
+  total += int(input("how many dimes?: ")) * 0.1
+  total += int(input("how many pennies?: ")) * 0.01
+  return total
+
+def is_transaction_successful(money_received, drink_cost):
+  """Return True when payment is accepted, or False if money is Insufficient"""
+  if money_received >= drink_cost:
+    change = round(money_received - drink_cost, 2)
+    print(f"Here is ${change} in change.")
+    global profit
+    profit += drink_cost
+    return True
+  else:
+    print("Sorry that's not enough money, Money refunded.")
+    return False
+
+def make_coffee(drink_name, order_ingredients):
+  """Deduct the required ingredients from the resources."""
+  for item in order_ingredients:
+    resources[item] -= order_ingredients[item]
+  print(f"Here is your {drink_name} ☕")
 
 is_on = True
 
@@ -51,4 +79,7 @@ while is_on:
     print(f"Money: ${profit}")
   else:
     drink = MENU[choice]
-    is_resource_sufficient(drink)
+    if is_resource_sufficient(drink["ingredients"]):
+      payment = process_coins()
+      if is_transaction_successful(payment, drink["cost"]):
+        make_coffee(choice, drink["ingredients"])
